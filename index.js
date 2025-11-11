1dec9ca544a68e43d6f9741976e15b490289327f
gambizardonkick <gambizardonkick@gmail.com>
2025-11-11 16:04:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-// index.js
-// Simple script to store and display keys
-
-// You can store your keys here
 const keys = [
   "key_1_example",
   "key_2_example",
   "key_3_example"
 ];
 
-// Display keys in order
-console.log("Stored Keys:\n");
 keys.forEach((key, index) => {
   console.log(`(${index + 1}) ${key}`);
 });
